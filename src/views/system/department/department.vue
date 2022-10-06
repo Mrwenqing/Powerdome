b33dd6b0af399c4c0336844eaa4e1d7d033d3dcc
别再相遇了 <lg002825@126.com>
2022-10-06 19:30:48 +0800

    feat(custom): 机构管理页面渲染

--- src/views/system/department/department.vue
+++ src/views/system/department/department.vue
@@ -1,20 +1,88 @@
 <template>
   <div>
     <el-button v-if="btn" type="primary">新增</el-button>
+    <el-table
+      :data="tableData"
+      style="width: 100%; margin-bottom: 20px"
+      row-key="id"
+      border
+      default-expand-all
+      :tree-props="{ children: 'children', hasChildren: 'hasChildren' }"
+    >
+      <el-table-column prop="date" label="部门名称"> </el-table-column>
+      <el-table-column prop="name" label="部门编码"> </el-table-column>
+      <el-table-column prop="address" label="部门电话"> </el-table-column>
+      <el-table-column prop="address" label="部门地址"> </el-table-column>
+      <el-table-column prop="address" label="序号"> </el-table-column>
+      <el-table-column label="操作">
+        <template slot-scope="scope">
+          <el-button size="mini" @click="handleEdit(scope.$index, scope.row)"
+            >编辑</el-button
+          >
+          <el-button
+            size="mini"
+            type="danger"
+            @click="handleDelete(scope.$index, scope.row)"
+            >删除</el-button
+          >
+        </template>
+      </el-table-column>
+    </el-table>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'DepartmentList',
+  name: "DepartmentList",
   data() {
     return {
-      btn: true
-    }
-  }
-}
+      tableData: [
+        {
+          id: 3,
+          date: "2016-05-01",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1519 弄",
+          children: [
+            {
+              id: 31,
+              date: "2016-05-01",
+              name: "王小虎",
+              address: "上海市普陀区金沙江路 1519 弄",
+              children: [
+                {
+                  id: 311,
+                  date: "2016-05-01",
+                  name: "王小虎",
+                  address: "上海市普陀区金沙江路 1519 弄",
+                },
+              ],
+            },
+          ],
+        },
+      ],
+    };
+  },
+  methods: {
+    load(tree, treeNode, resolve) {
+      setTimeout(() => {
+        resolve([
+          {
+            id: 31,
+            date: "2016-05-01",
+            name: "王小虎",
+            address: "上海市普陀区金沙江路 1519 弄",
+          },
+          {
+            id: 32,
+            date: "2016-05-01",
+            name: "王小虎",
+            address: "上海市普陀区金沙江路 1519 弄",
+          },
+        ]);
+      }, 1000);
+    },
+  },
+};
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
