1fba184ce13690d88fb859900f0de222aeaa7ca8
别再相遇了 <lg002825@126.com>
2022-10-06 18:47:15 +0800

    style(custom): 首页页面渲染

--- src/main.js
+++ src/main.js
@@ -1,25 +1,25 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
 
-import './permission'
-import './directive/buttonPermission'
+import "./permission";
+import "./directive/buttonPermission";
 
-import ElementUI from 'element-ui'
-import 'element-ui/lib/theme-chalk/index.css'
-
+import ElementUI from "element-ui";
+import "element-ui/lib/theme-chalk/index.css";
+import "./style/reset.css";
 // 解决menu菜单收起文字不隐藏的问题
-import Fragment from 'vue-fragment'
+import Fragment from "vue-fragment";
 
-Vue.use(ElementUI)
+Vue.use(ElementUI);
 
-Vue.use(Fragment.Plugin)
+Vue.use(Fragment.Plugin);
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
-  render: h => h(App)
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
--- src/views/home.vue
+++ src/views/home.vue
@@ -1,35 +1,100 @@
 <template>
   <div>
-    <ul>
+    <!-- <ul>
       <li v-for="(item,index) in tags" :key="index">
         <span>{{ item.title }}</span>
         <i v-if="index > 1" class="el-icon-close close" @click="handleClose(index)"></i>
       </li>
-    </ul>
+    </ul> -->
+    <!-- <h1>home</h1> -->
+    <div class="d1">
+      <div class="d2">
+        <div class="p1">
+          <h1>0</h1>
+          <p>今日已支付订单</p>
+        </div>
+        <div class="p1">
+          <h1>0</h1>
+          <p>今日代发货</p>
+        </div>
+      </div>
+      <div class="d2">
+        <div class="p1">
+          <h1>0</h1>
+          <p>今日已发货订单</p>
+        </div>
+        <div class="p2">
+          <h1>0</h1>
+          <p>今日异常</p>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Home',
-  computed: {
-    tags() {
-      return this.$store.getters.tags
-    }
-  },
-  methods: {
-    handleClose(index) {
-      this.$store.commit('Tags/removeTags', index)
-    }
+  name: "Home",
+  // computed: {
+  //   tags() {
+  //     return this.$store.getters.tags
+  //   }
+  // },
+  // methods: {
+  //   handleClose(index) {
+  //     this.$store.commit('Tags/removeTags', index)
+  //   }
+  // }
+};
+</script>
+<style scoped lang="scss">
+.d1 {
+  width: 100%;
+  height: 400px;
+  // background-color: aqua;
+  /* display: flex; */
+  box-shadow: none;
+}
+.d2 {
+  width: 100%;
+  height: 200px;
+  display: flex;
+  padding: 20px;
+  // background-color: blue;
+  box-sizing: border-box;
+}
+.p2 {
+  background-color: #dd4b39 !important;
+  p {
+    background-color: #c64333 !important;
   }
 }
-</script>
-
-<style scoped>
-ul {
+.p1,
+.p2 {
+  margin-left: 10px;
+  width: 50%;
+  height: 100%;
+  background-color: #00c0ef;
+  border-radius: 20px;
+  p,
+  h1 {
+    margin-top: 30px;
+    margin-left: 30px;
+    color: #fff;
+    font-size: 30px;
+  }
+  p {
+    margin-top: 40px !important;
+    margin-left: 0;
+    height: 40px;
+    margin-top: 30px;
+    font-size: 22px;
+    background-color: #3399ff;
+  }
+}
+/* ul {
   list-style: none;
   display: flex;
-
 }
 
 ul li {
@@ -56,5 +121,5 @@ ul li {
 
 .close:hover {
   background-color: #cac6c6;
-}
+} */
 </style>
